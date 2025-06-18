c4dfe590a9195d71e62472deb35bc925d3b57295
yousri <129407665+yousriB@users.noreply.github.com>
2025-06-18 11:44:09 +0100

    fix the error building

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,16 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
   },
 }));
